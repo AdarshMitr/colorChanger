c4c4c06b29c848ecb73adb181168d6ba7292f7ba
AdarshMitr <adarshkumar019@gmail.com>
2023-11-05 12:05:06 +0530

    text styles added

--- changebgcolor.js
+++ changebgcolor.js
@@ -1,16 +1,20 @@
+const btns = document.querySelectorAll(".color-btn");
+const body = document.querySelector("body");
+// const textcolor=document.querySelector('.para');
 
-const btns=document.querySelectorAll('.color-btn');
-const body=document.querySelector('body');
+btns.forEach((btn) => {
+  // console.log(btn)
 
-btns.forEach((btn)=>{
-// console.log(btn)
-
-btn.addEventListener('click',function(e){
+  btn.addEventListener("click", function (e) {
     //console.log(e.target.id)
-    body.style.backgroundColor=e.target.id
-  
-    
-});
-
-
+    body.style.backgroundColor = e.target.id;
+  });
+  btn.addEventListener("click", function (f) {
+    //console.log(e.target.id)
+    if (f.target.id == "white" || f.target.id == "yellow") {
+      body.style.color = "black";
+    } else {
+      body.style.color = "white";
+    }
+  });
 });
